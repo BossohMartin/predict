c266dc0b740c9394f6564722866a7c42ae4e0ff3
Amoassi Martin Bossoh <bossoh1909@hotmail.fr>
2022-12-12 20:57:37 +0100

    suppression des bugs sur api

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import pickle
 import pandas as pd
-from flask import Flask, jsonify, render_template, request
-from lightgbm import LGBMClassifier
-
+from flask import Flask, jsonify
+import logging
 
 DEBUG = True
 app = Flask(__name__)
@@ -15,42 +11,77 @@ def read_dataframe(filename):
     data.drop(['Unnamed: 0', 'Unnamed: 0.1'],axis=1,inplace=True)
     data['SK_ID_CURR'] = data['SK_ID_CURR'].apply(int)
     data.drop(['TARGET'],axis=1,inplace=True)
+    app.logger.info("succesfully loaded dataset:{}".format(filename))
     return data
 
 
 def load_model(modelname):
-    modelname = 'LGBMClassifier'
-    return pickle.load(open("LGBMClassifier.pkl",'rb'))
+    model = pickle.load(open("{}.pkl".format(modelname),'rb'))
+    model.feature_name_
+    model.set_params(n_jobs=1)
+    app.logger.info("succesfully loaded model:{}".format(modelname))
+    return model
 
 
-model = load_model(LGBMClassifier)
+model = load_model("LGBMClassifier")
 data = read_dataframe('data_train_1.csv')
 
+c_id = data['SK_ID_CURR'][0]
+X = data[data['SK_ID_CURR'] == c_id]
+logging.error("id:{}".format(c_id))
+# check if the the id exist in the dataframe
+if X.shape[0] == 1:
+    classofinterest = 0 # depending if you want the positive or negative prediction
+    y_proba = model.predict_proba(X)[0] # no need to do a predict and a predict proba
+    proba = y_proba[classofinterest]
+    y = proba.argmax() # by definition the max value of class probability is the predicted class
 
+    result = {
+                'prediction' : int(y),
+                'proba' : float(proba),
+                'id': c_id
+                }
+    logging.error("result:{}".format(result))
+
+logging.error("done with setup")
 
 @app.route('/')
 def documentation():
     return jsonify('bienvenue dans mon application')
 
-
-@app.route('/<ID_client>', methods=["POST", "GET"])
-def predict_proba(ID_client):
-    X = data[data['SK_ID_CURR'] == int(ID_client)]
+@app.route('/predict/<int:c_id>', methods=["GET"])
+def predict_proba(c_id):
+    # the id should not be include in the x
+    # and the code could be like that
+    # id = int(ID_client)
+    # data = data.set_index("SK_ID_CURR") # this should be done where you load your dataframe so it is done only once and not every single time you call predict_proba
+    # x = data.loc[id] # here the id in not in x valuesn but only as index
+    # y_proba = model.predict_proba(x)[0]
+    X = data[data['SK_ID_CURR'] == c_id]
+    # check if the the id exist in the dataframe
     if X.shape[0] == 1:
-        prediction = int(model.predict(X)[0])
-        proba = model.predict_proba(X)[0][prediction]
+        model.classes_
+        logging.info('enter predict')
+        classofinterest = 0 # depending if you want the positive or negative prediction
+        y_proba = model.predict_proba(X)[0] # no need to do a predict and a predict proba
+        logging.info('end predict')
+        proba = y_proba[classofinterest]
+        y = proba.argmax() # by definition the max value of class probability is the predicted class
 
-        dict_final = {
-            'prediction' : int(prediction),
-            'proba' : float(proba)
-            }
-
-        return jsonify(dict_final)
+        result = {
+                    'prediction' : int(y),
+                    'proba' : float(proba),
+                    'id': c_id
+                    }
     else:
-        return (False)
-if __name__ == '__main__':
-    app.run()
+        result = False
+    logging.info('jsonify')
+    return jsonify(result)
+
+@app.route('/myid/<myid>')
+def myid(myid):
+    return myid
 
 
-
-
+#if __name__ == '__main__':
+#    app.run()
